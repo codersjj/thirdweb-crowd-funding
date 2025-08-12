bd11985c8628405ecab4e7a86c21292a1359cccd
codersjj <shanewestlife@outlook.com>
2025-08-12 22:21:18 +0800

    feat: add crowdfunding contract integration and display campaigns in Home component

--- web3cfapp/src/app/page.tsx
+++ web3cfapp/src/app/page.tsx
@@ -1,8 +1,40 @@
+'use client'
+
+import { getContract } from "thirdweb";
+import { client } from "./client";
+import { baseSepolia } from "thirdweb/chains";
+import { CROWDFUNDING_CONTRACT } from "./constants/contracts";
+import { useReadContract } from "thirdweb/react";
+
+const contract = getContract({
+  client: client,
+  chain: baseSepolia,
+  address: CROWDFUNDING_CONTRACT
+})
+
 export default function Home() {
+  const { data: campaigns, isLoading } = useReadContract({
+    contract,
+    method: "function getAllCampaigns() external view returns ((address campaignAddress, address owner, string name, uint256 creationTime)[])",
+    params: []
+  })
+    console.log("🚀 ~ Home ~ campaigns, isLoading:", campaigns, isLoading)
+
   return (
     <main className="mx-auto max-w-7xl px-4 mt-4 sm:px-6 lg:px-8">
       <div className="py-10">
         <h1 className="mb-4 text-4xl font-bold">Campaigns:</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {!isLoading && campaigns && campaigns.length ? (
+            campaigns.map(campaign => (
+              <div key={campaign.campaignAddress}>
+                <p>{campaign.name}</p>
+              </div>
+            ))
+          ) : (
+            <p>No campaigns found</p>
+          )}
+        </div>
       </div>
     </main>
   );
